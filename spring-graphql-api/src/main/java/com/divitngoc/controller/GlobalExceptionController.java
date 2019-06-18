c13bd4c948513e4113ad535ad590b2f200d26c26
Divit Bui <divitngoc@gmail.com>
2019-06-18 21:22:06 +0100

    Add GlobalExceptionController
    This will be used to handle exceptions thrown

---
+++ spring-graphql-api/src/main/java/com/divitngoc/controller/GlobalExceptionController.java
@@ -0,0 +1,11 @@
+package com.divitngoc.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionController {
+
+}
